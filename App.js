19532afa11395587e264abe521f7380fe8766249
rajandangi <rajandangi649@gmail.com>
2025-01-04 23:29:52 +1030

    Refactor App.js to use nested div structure for heading and subheading

--- App.js
+++ App.js
@@ -1,4 +1,20 @@
-const heading = React.createElement('h1', { id: "heading" }, "Hello from React");
+/**
+ * <div id="parent">
+ *  <div id="child">
+ *   <h1>Hello from React</h1>
+ *   <h2> This is h2 title</h2>
+ *  </div>
+ * </div>
+ */
+
+const heading = React.createElement('div', { id: "parent" },
+    React.createElement("div", { id: "child" },
+        [
+            React.createElement("h1", {}, "Hello from React"),
+            React.createElement("h2", {}, "This is h2 title")
+        ]
+    )
+);
 console.log(heading);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
