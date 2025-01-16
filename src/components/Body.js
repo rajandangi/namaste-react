6526e56faeed6bf171308309f13e24b3f8a8a7df
rajandangi <rajandangi649@gmail.com>
2025-01-16 12:51:00 +1030

    Implement Shimmer loading component, update Body to fetch restaurant data from JSON server, and remove hardcoded restaurant data; adjust package.json for json-server integration and add shimmer styles in index.css.

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,16 +1,27 @@
 import RestaurantCard from './RestaurantCard';
-import { useState } from 'react';
-import { restaurantData } from '../utils/restaurantData';
+import Shimmer from './Shimmer';
+import { useState, useEffect } from 'react';
 
 const Body = () => {
-    const [restaurants, setRestaurants] = useState(restaurantData);
+    const [restaurants, setRestaurants] = useState([]);
 
     const filterTopRatedRestaurants = () => {
         const topRatedRestaurants = restaurants.filter((restaurant) => restaurant.data.rating > 4);
         setRestaurants(topRatedRestaurants);
     }
 
-    return (
+    async function fetchRestaurants() {
+        const response = await fetch('http://localhost:3001/restaurants');
+        const data = await response.json();
+        setRestaurants(data);
+    }
+
+    // useEffect is a hook that runs after the first render of the component
+    useEffect(() => {
+        fetchRestaurants();
+    }, []);
+
+    return (restaurants.length === 0 ? <Shimmer /> :
         <div className="body">
             <div className='filter'>
                 <button className='filter-btn' onClick={filterTopRatedRestaurants}>Top Rated Restaurants</button>
